484a2370abf98f25f42c8fb79d55109da0e15968
Jung-won-seok <ukksj0621@naver.com>
2025-01-26 16:03:46 +0900

    체육복

--- src/main/java/programmers/체육복.java
+++ src/main/java/programmers/체육복.java
@@ -9,30 +9,42 @@ public class 체육복 {
     public int solution(int n, int[] lost, int[] reserve) {
 
         int[] student = new int[n];
-        for (int i = 1; i <= n; i++) {
-            student[i - 1] = 1;
+        for (int i = 0; i < n; i++) {
+            student[i] = 1;
         }
 
+        //여벌 체육복이 있는 학생 = 2
         for (int i = 0; i < reserve.length; i++) {
             student[reserve[i]-1]++;
         }
 
+        //체육복을 잃어버린 학생 = 0
         for (int i = 0; i < lost.length; i++) {
             student[lost[i]-1]--;
         }
 
+        //체육복 빌려주기
+        //[1, 0, 2, 0, 1]
         for (int i = 0; i < n; i++) {
             if (student[i] == 0) {
-                if (i >= 1 || i < n - 1) {
-                    if (student[i - 1] == 2 || student[i + 1] == 2) {
-
-                    }
+                if (i>0 && student[i - 1] == 2) {
+                    student[i] = 1;
+                    student[i - 1] = 1;
+                } else if (i<n-1 && student[i + 1] == 2) {
+                    student[i+1] = 1;
+                    student[i] = 1;
                 }
 
             }
         }
 
         int answer = 0;
+        for (int i = 0; i < n; i++) {
+            if (student[i] >= 1) {
+                answer++;
+            }
+        }
+
         return answer;
     }
 }
