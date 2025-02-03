fc81a7eb96a33dae219c6bfcc28cd98bcdceb108
Jung-won-seok <ukksj0621@naver.com>
2025-02-04 01:03:10 +0900

    입국심사

--- src/main/java/programmers/입국심사.java
+++ src/main/java/programmers/입국심사.java
@@ -1,6 +1,6 @@
 package programmers;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class 입국심사 {
     public long solution(int n, int[] times) {
@@ -8,15 +8,30 @@ public class 입국심사 {
         //times[] = [7, 10]
         //return = 28
 
-        int cnt = 0;
-        ArrayList<Integer> arr = new ArrayList<>();
-        for(int i=0 ; i<times.length ; i++) {
-            arr.add(times[i]);
+        Arrays.sort(times);
+        long min = 1;
+
+        long max = (long) times[times.length - 1] * n;
+        long mid = 0;
+        long sum;
+        long answer = max;
+
+        while (min <= max) {
+            sum = 0;
+            mid = (min + max) / 2;
+
+            for (int i = 0; i < times.length; i++) {
+                sum = sum + mid / times[i];
+            }
+
+            if (sum >= n) {
+                answer = mid;
+                max = mid - 1;
+            } else {
+                min = mid + 1;
+            }
         }
-
-
-
-        long answer = 0;
         return answer;
+
     }
 }
