31e909f5b78bef8c0faca420b250b46789e251a9
Jung-won-seok <ukksj0621@naver.com>
2025-02-03 23:43:35 +0900

    입국심사

---
+++ src/main/java/programmers/입국심사.java
@@ -0,0 +1,22 @@
+package programmers;
+
+import java.util.ArrayList;
+
+public class 입국심사 {
+    public long solution(int n, int[] times) {
+        //n=6
+        //times[] = [7, 10]
+        //return = 28
+
+        int cnt = 0;
+        ArrayList<Integer> arr = new ArrayList<>();
+        for(int i=0 ; i<times.length ; i++) {
+            arr.add(times[i]);
+        }
+
+
+
+        long answer = 0;
+        return answer;
+    }
+}
