a2bc9db18e1c5ebf9bd92e7b7e53a435c02ac50e
Jung-won-seok <ukksj0621@naver.com>
2025-02-16 22:46:15 +0900

    가장 먼 노드

--- src/main/java/programmers/가장먼노드.java
+++ src/main/java/programmers/가장먼노드.java
@@ -1,4 +1,20 @@
 package programmers;
 
 public class 가장먼노드 {
+    public int solution(int n, int[][] edge) {
+        //n = 6, edge[][] = [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]], return = 3
+        //[3, 6]
+        //[4, 3]
+        //[3, 2]
+        //[1, 3]
+        //[1, 2]
+        //[2, 4]
+        //[5, 2]
+
+
+        //2, 3, 4, 5, 6과 1의 거리를 찾는다.
+        //가장 먼 것들의 갯수를 return 한다.
+        int answer = 0;
+        return answer;
+    }
 }
