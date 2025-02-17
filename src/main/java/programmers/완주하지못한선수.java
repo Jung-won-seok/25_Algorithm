a6cb28023a7f49441520fe915d630a3e2ba971dd
Jung-won-seok <ukksj0621@naver.com>
2025-02-17 23:19:41 +0900

    완주하지 못한 선수

---
+++ src/main/java/programmers/완주하지못한선수.java
@@ -0,0 +1,18 @@
+package programmers;
+
+import java.util.Arrays;
+
+class 완주하지못한선수 {
+    public String solution(String[] participant, String[] completion) {
+
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        int i = 0;
+        for(i=0;i<completion.length;i++)
+            if(!participant[i].equals(completion[i]))
+                break;
+
+        return participant[i];
+    }
+}
