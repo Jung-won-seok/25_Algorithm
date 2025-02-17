aab337c225c398fe4391f5df53639ce75e1a6629
Jung-won-seok <ukksj0621@naver.com>
2025-02-17 23:40:21 +0900

    완주하지 못한 선수

---
+++ src/main/java/programmers/완주하지못한선수_2.java
@@ -0,0 +1,28 @@
+package programmers;
+
+import java.util.HashMap;
+
+public class 완주하지못한선수_2 {
+    class Solution{
+        public String solution(String[] participant, String[] completion) {
+            String answer = "";
+
+            HashMap<String, Integer> map = new HashMap<>();
+
+            for (String p : participant) {
+                map.put(p, map.getOrDefault(p, 0) + 1);
+            }
+
+            for (String c : completion) {
+                map.put(c, map.get(c) - 1);
+            }
+
+            for (String key : map.keySet()) {
+                if (map.get(key) != 0) {
+                    return key;
+                }
+            }
+            return answer;
+        }
+    }
+}
