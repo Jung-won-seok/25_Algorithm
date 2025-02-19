9384f003266dcb8b14c91ca8bbe954373497006f
Jung-won-seok <ukksj0621@naver.com>
2025-02-19 20:56:52 +0900

    완주하지 못한 선수

--- src/main/java/programmers/완주하지못한선수_2.java
+++ src/main/java/programmers/완주하지못한선수_2.java
@@ -10,8 +10,8 @@ public class 완주하지못한선수_2 {
             HashMap<String, Integer> map = new HashMap<>();
 
             //getOrDefault 메서드 -> 해시맵에 존재하는 이름이면 값을 증가, 존재하지 않으면 0을 기본값 설정 후 1 증가
-            for (String p : participant) {
-                map.put(p, map.getOrDefault(p, 0) + 1);
+            for (int i = 0; i < participant.length; i++) {
+                map.put(participant[i], map.getOrDefault(participant[i], 0) + 1);
             }
 
             //완주자의 이름의 값을 해시맵에서 감소
