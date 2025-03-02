56e473b6a47ad1e77d131f677d2f0dd0d5c44794
Jung-won-seok <ukksj0621@naver.com>
2025-03-02 22:10:36 +0900

    N으로 표현

--- src/main/java/programmers/N으로표현.java
+++ src/main/java/programmers/N으로표현.java
@@ -1,30 +1,23 @@
 package programmers;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 public class N으로표현 {
     //N=5, number=12, return=4
     //N=2, number=11, return=3
 
     public int solution(int N, int number) {
+        //N=5, number=12, return=4
 
-        List<Set<Integer>> box = new ArrayList<>();
-        for (int i = 1; i <= 8; i++) {
-            Set<Integer> set = new HashSet<>();
-            String nn = Integer.toString(N).repeat(i);
-            set.add(Integer.parseInt(nn));
-            box.add(set);
-        }
-
-        for (int i = 2; i <= 8; i++) {
-
+        int cnt = 1;
+        for (int i = 0; i < 100; i++) {
+            int plus = N + N;
+            int minus = N - N;
+            int gob = N * N;
+            int division = N / N;
+            int overlap = N * 10;
         }
 
         int answer = 0;
         return answer;
-
     }
 }
