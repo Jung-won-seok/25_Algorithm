a964996235d34ddb42c59b9750c95140c269f050
Jung-won-seok <ukksj0621@naver.com>
2025-03-06 00:53:02 +0900

    퇴사

--- src/main/java/beak/beak_14501.java
+++ src/main/java/beak/beak_14501.java
@@ -1,7 +1,18 @@
 package beak;
 
+import java.util.Scanner;
+
 public class beak_14501 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.nextLine();
+        int[][] day = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            day[i][0] = sc.nextInt();
+            day[i][1] = sc.nextInt();
+        }
+
 
     }
 }
