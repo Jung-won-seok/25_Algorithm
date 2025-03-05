a632fd4988b991448114cb6ffc3516c649bfaf6d
Jung-won-seok <ukksj0621@naver.com>
2025-03-05 22:09:27 +0900

    주사위 굴리기

--- src/main/java/beak/beak_14499.java
+++ src/main/java/beak/beak_14499.java
@@ -15,7 +15,6 @@ public class beak_14499 {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
                 cordinate[i][j] = sc.nextInt();
-                cordinate[i][j] = sc.nextInt();
             }
         }
 
@@ -24,16 +23,27 @@ public class beak_14499 {
             move[i] = sc.nextInt();  //move[] = {4 4 4 1 3 3 3 2}
         }
 
-
         int[] dice = new int[6];
         for (int i = 0; i < dice.length; i++) {
             dice[i] = 0;
         }
 
-
         //주사위 굴리기
         for (int i = 0; i < move.length; i++) {
             int[] newDice = new int[6];
+
+            // 좌표 이동
+            if (move[i] == 1 && y + 1 < M) {
+                y++;
+            } else if (move[i] == 2 && y - 1 >= 0) {
+                y--;
+            } else if (move[i] == 3 && x - 1 >= 0) {
+                x--;
+            } else if (move[i] == 4 && x + 1 < N) {
+                x++;
+            } else {
+                continue;
+            }
 
             switch (move[i]) {
                 case 1: // 동쪽으로 굴림
@@ -75,10 +85,17 @@ public class beak_14499 {
 
             // 주사위 상태 업데이트
             dice = newDice;
+
+            // 바닥 숫자 복사 및 출력
+            if (cordinate[x][y] == 0) {
+                cordinate[x][y] = dice[5];
+            } else {
+                dice[5] = cordinate[x][y];
+                cordinate[x][y] = 0;
+            }
+
+            // 윗면 숫자 출력
+            System.out.println(dice[0]);
         }
-
-
-
-
     }
 }
