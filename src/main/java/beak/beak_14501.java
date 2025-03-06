6fbd5dc85fbfbef040451d8d72938eefc5afaf26
Jung-won-seok <ukksj0621@naver.com>
2025-03-06 13:25:17 +0900

    퇴사

--- src/main/java/beak/beak_14501.java
+++ src/main/java/beak/beak_14501.java
@@ -13,6 +13,14 @@ public class beak_14501 {
             day[i][1] = sc.nextInt();
         }
 
-
+        int[] dp = new int[N + 1];
+        for (int i = 0; i < N; i++) {
+            int nextDay = i + day[i][0];
+            if (nextDay <= N) {
+                dp[nextDay] = Math.max(dp[nextDay], dp[i] + day[i][1]);
+            }
+            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
+        }
+        System.out.println(dp[N]);
     }
 }
