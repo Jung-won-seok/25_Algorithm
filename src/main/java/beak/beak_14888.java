5eab7de9c5f8b21dabf15655bc9e9820ea57707f
Jung-won-seok <ukksj0621@naver.com>
2025-03-15 22:37:29 +0900

    연산자 끼워넣기

--- src/main/java/beak/beak_14888.java
+++ src/main/java/beak/beak_14888.java
@@ -1,7 +1,30 @@
 package beak;
 
-public class beak_14888 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class beak_14888 {
+    public static int N;
+    public static int[] operator = new int[4]; // + - * /
+    public static int[] number;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < 4; i++) {
+            operator[i] = sc.nextInt();
+        }
+
+
+    }
+
+    public static void dfs(int depth, int num) {
+        if (depth == N) {
+
+        }
     }
 }
