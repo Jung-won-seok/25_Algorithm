ca409118fe66200e4bd2d0d9fb4e9d91bc8419be
Jung-won-seok <ukksj0621@naver.com>
2025-03-18 22:36:36 +0900

    연산자 끼워넣기

--- src/main/java/beak/beak_14888.java
+++ src/main/java/beak/beak_14888.java
@@ -5,26 +5,26 @@ import java.util.Scanner;
 public class beak_14888 {
     public static int MAX = Integer.MIN_VALUE;	// 최댓값
     public static int MIN = Integer.MAX_VALUE;	// 최솟값
-    public static int[] operator = new int[4];	// 연산자 개수
+    public static int[] operator = new int[4];	// 연산자 개수 + - * /
     public static int[] number;					// 숫자
     public static int N;						// 숫자 개수
 
     public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        N = in.nextInt();
+        N = sc.nextInt();
         number = new int[N];
 
 
         // 숫자 입력
         for (int i = 0; i < N; i++) {
-            number[i] = in.nextInt();
+            number[i] = sc.nextInt();
         }
 
         // 연산자 입력
         for (int i = 0; i < 4; i++) {
-            operator[i] = in.nextInt();
+            operator[i] = sc.nextInt();
         }
 
         dfs(number[0], 1);
@@ -42,10 +42,8 @@ public class beak_14888 {
         }
 
         for (int i = 0; i < 4; i++) {
-            // 연산자 개수가 1개 이상인 경우
             if (operator[i] > 0) {
 
-                // 해당 연산자를 1 감소시킨다.
                 operator[i]--;
 
                 switch (i) {
@@ -56,7 +54,6 @@ public class beak_14888 {
                     case 3:	dfs(num / number[idx], idx + 1);	break;
 
                 }
-                // 재귀호출이 종료되면 다시 해당 연산자 개수를 복구한다.
                 operator[i]++;
             }
         }
