8d59499de6a34b2849b48a61acc50a0f52759b39
Jung-won-seok <ukksj0621@naver.com>
2025-03-21 23:09:54 +0900

    테트로미노

--- src/main/java/beak/beak_14500.java
+++ src/main/java/beak/beak_14500.java
@@ -15,6 +15,61 @@ public class beak_14500 {
             }
         }
 
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M-3; j++) {
+                int first = board[i][j] + board[i][j + 1] + board[i][j + 2] + board[i][j + 3];
+                if (first > max) {
+                    max = first;
+                }
+            }
+        }
+
+        for (int i = 0; i < N - 3; i++) {
+            for (int j = 0; j < M; j++) {
+                int first_low = board[i][j] + board[i+1][j] + board[i+2][j] + board[i+3][j];
+                if (first_low > max) {
+                    max = first_low;
+                }
+            }
+        }
+
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < M - 1; j++) {
+                int second = board[i][j] + board[i+1][j] + board[i][j+1] + board[i+1][j+1];
+                if (second > max) {
+                    max = second;
+                }
+            }
+        }
+
+        for (int i = 0; i < N-2 ; i++) {// 0~2
+            for (int j = 0; j < M - 1; j++) {// 0~3
+                int thrid = board[i][j] + board[i+1][j] + board[i+2][j] + board[i+2][j+1];
+                if (thrid > max) {
+                    max = thrid;
+                }
+            }
+        }
+
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < M - 2; j++) {
+                int third_2 = board[i][j] + board[i][j+1] + board[i][j+2] + board[i+1][j+2];
+                if (third_2 > max) {
+                    max = third_2;
+                }
+            }
+        }
+
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < M - 2; j++) {
+                int thrid_3 = board[i][j] + board[i+1][j] + board[i][j+1] + board[i][j+2];
+                if (thrid_3 > max) {
+                    max = thrid_3;
+                }
+            }
+        }
+
 
 
     }
